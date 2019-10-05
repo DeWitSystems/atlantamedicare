ccba8f90c2582c35ecf4ea6c6b3565fd8a6fcc95
northphex <54204550+northphex@users.noreply.github.com>
2019-10-05 19:26:48 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -71,10 +71,14 @@
                         <p><?php echo "250 ladphroad" ;?></p>
                         <p><?php echo "Bangkok Thailand" ;?></p>
                      </div>
-                </div>
+                </div><!-- .site-location-->
+                
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'atlantamedicare' ); ?></button>
-			<?php
+                        <div class="border-menu"></div>
+                        <div class="border-menu"></div>
+                        <div class="border-menu"></div>
+                            <?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
